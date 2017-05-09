cdfa6fae5c2daf13e16c0b14ebac3f0c4197a581
Soumya Deb <debloper@gmail.com>
2017-05-10 01:17:57 +0530

    refactor(app): replaced template engine, added UI framework and implemented basic routing

--- app.js
+++ app.js
@@ -4,15 +4,14 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
+var engine = require('express-dot-engine');
 
 var app = express();
 
 // view engine setup
+app.engine('dot', engine.__express);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'dot');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,8 +21,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/', require('./routes/index'));
+app.use('/profile', require('./routes/profile'));
+app.use('/dashboard', require('./routes/dashboard'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET dashboard. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('dashboard', { title: 'XpleX: Dashboard' });
 });
 
 module.exports = router;
